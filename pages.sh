f584db5dbe30485f595c06dca6c770d196005ce2
Cristobal Hall-Ramos <crishallramos@yahoo.com>
2022-03-13 00:38:39 -0500

    script2

--- pages.sh
+++ pages.sh
@@ -3,4 +3,6 @@ git checkout -b gh-pages && \
 npm run build && \
 rm -rf public scraper src package-lock.json package.json README.md tsconfig.json && \
 mv build/* . && \
-rm -rf build
+rm -rf build && \
+git add . && \
+git commit -m "gh-pages"
